64eb9cc681c44e149be6ecdd1da3a473e837ac46
Teodor <elrynsier@gmail.com>
2025-03-04 15:45:26 +0200

    localStorage of themes and changing

--- taskmate/src/components/Header.jsx
+++ taskmate/src/components/Header.jsx
@@ -1,20 +1,30 @@
+import { useEffect, useState } from 'react';
 import Logo from '../assets/logo.png';
 
 
 export default function Header() {
-  return (
+    const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || 'light');
+
+
+    useEffect(() => {
+        document.documentElement.classList.remove("light", "medium", "dark", "gOne", "gTwo", "gThree");
+        document.documentElement.classList.add(theme);
+        localStorage.setItem('theme', JSON.stringify(theme))
+    }, [theme])
+
+return (
     <header>
         <div className="logo">
             <img src={Logo} alt="Taskmate Logo" />
             <span>Taskmate</span>
         </div>
         <div className='themeSelector'>
-            <span className='light'></span>
-            <span className='medium'></span>
-            <span className='dark'></span>
-            <span className='gOne'></span>
-            <span className='gTwo'></span>
-            <span className='gThree'></span>
+            <span onClick={() => setTheme('light')} className='light'></span>
+            <span onClick={() => setTheme('medium')} className='medium'></span>
+            <span onClick={() => setTheme('dark')} className='dark'></span>
+            <span onClick={() => setTheme('gOne')} className='gOne'></span>
+            <span onClick={() => setTheme('gTwo')} className='gTwo'></span>
+            <span onClick={() => setTheme('gThree')} className='gThree'></span>
         </div>
     </header>
   )
