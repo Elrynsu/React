a510d0fae028a7fa37a4f38759ce69c2296092d4
Teodor <elrynsier@gmail.com>
2025-03-07 15:19:03 +0200

    updated testimonials info /  functionality

--- EvCars/src/pages/Home/HomePage.jsx
+++ EvCars/src/pages/Home/HomePage.jsx
@@ -1,11 +1,15 @@
+import Faq from "./components/Faq";
 import FeaturedProducts from "./components/FeaturedProducts";
 import Hero from "./components/Hero";
+import Testimonials from "./components/Testimonials";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <FeaturedProducts />
+      <Testimonials />
+      <Faq />
     </main>
   )
 }
