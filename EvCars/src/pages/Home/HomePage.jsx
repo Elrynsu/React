34ef02f2777eb394e5258794e98168c9849146d5
Teodor <elrynsier@gmail.com>
2025-03-07 13:15:32 +0200

    static featured Cars improvements

--- EvCars/src/pages/Home/HomePage.jsx
+++ EvCars/src/pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
+import FeaturedProducts from "./components/FeaturedProducts";
 import Hero from "./components/Hero";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <FeaturedProducts />
     </main>
   )
 }
--- EvCars/src/pages/Home/components/Hero.jsx
+++ EvCars/src/pages/Home/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import evCarStore from '../../../assets/EvCarStore.webp'
 
 export default function Hero() {
@@ -6,7 +7,7 @@ export default function Hero() {
         <div className="text my-5">
             <h1 className="text-5xl font-bold">The Ultimate Ev Cars Store</h1>
             <p className="text-2xl my-7 px-1 dark:text-slate-300">Ev Cars is the world's most popular and authoritative source for electric vehicles. Find ratings and access to the newest cars digitally.</p>
-            <a href="" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Look for cars here</a>
+            <Link to="/products" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Look for cars here</Link>
         </div>
         <div className="visual my-5 lg:max-w-xl">
             <img className="rounded-lg max-h-full" src={evCarStore} alt="Ev Cars Section" />
