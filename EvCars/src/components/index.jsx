040ba30eab5ff0ffe800f8c16b3e0be1b19b2c1b
Teodor <elrynsier@gmail.com>
2025-03-09 20:51:35 +0200

    scroll to Top with useLocation

--- EvCars/src/components/index.jsx
+++ EvCars/src/components/index.jsx
@@ -1,3 +1,6 @@
 export { default as Header } from './Layouts/Header';
 export { default as Footer } from './Layouts/Footer';
+
 export { default as ProductCard } from './Elements/ProductCard';
+
+export { default as ScrollToTop } from './Other/ScrollToTop';
--- EvCars/src/main.jsx
+++ EvCars/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { ScrollToTop } from './components/index.jsx'
 
 createRoot(document.getElementById('root')).render(
     <Router>
+        <ScrollToTop />
         <App />
     </Router>
 )
